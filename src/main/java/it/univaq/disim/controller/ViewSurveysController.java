3297d7d8befb2afd6c2f3565dba4a0e0bce74550
Raffaele Limongi <raffaele830@gmail.com>
2020-02-20 19:17:24 +0100

    aggiornato il download del csv!!

--- src/main/java/it/univaq/disim/controller/ViewSurveysController.java
+++ src/main/java/it/univaq/disim/controller/ViewSurveysController.java
@@ -96,31 +96,21 @@ public class ViewSurveysController extends HttpServlet {
             File downloadFile = new File(filePath);
             FileInputStream inStream = new FileInputStream(downloadFile);
 
-            // if you want to use a relative path to context root:
-            String relativePath = getServletContext().getRealPath("");
-            System.out.println("relativePath = " + relativePath);
-
-            // obtains ServletContext
             ServletContext context = getServletContext();
 
-            // gets MIME type of the file
             String mimeType = context.getMimeType(filePath);
             if (mimeType == null) {
-                // set to binary type if MIME mapping not found
                 mimeType = "application/octet-stream";
             }
             System.out.println("MIME type: " + mimeType);
 
-            // modifies response
             response.setContentType(mimeType);
             response.setContentLength((int) downloadFile.length());
 
-            // forces download
             String headerKey = "Content-Disposition";
             String headerValue = String.format("attachment; filename=\"%s\"", downloadFile.getName());
             response.setHeader(headerKey, headerValue);
 
-            // obtains response's output stream
             OutputStream outStream = response.getOutputStream();
 
             byte[] buffer = new byte[4096];
